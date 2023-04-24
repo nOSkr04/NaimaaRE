00ba4f13c2984d0b9393e4e44b53162432cbe408
nOSkr <118327643+nOSkr@users.noreply.github.com>
2023-04-24 19:23:02 +0800

    a

---
+++ src/sheets/IncomeSheet.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { memo } from "react";
+
+const IncomeSheet = memo(() => {
+    return (
+      <View>
+        <Text>IncomeSheet</Text>
+      </View>
+    );
+  });
+
+  IncomeSheet.displayName="IncomeSheet";
+
+export { IncomeSheet };
+
+const styles = StyleSheet.create({});
